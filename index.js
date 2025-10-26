d7a9327cda7fa41c8e02233e851eb397586e82b1
John Doe <johndoe@example.com>
2025-10-26 18:51:03 +0200

    removed wrong importrs

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config({
-	path: process.env.NODE_ENV === "production" ? ".env.production" : ".env",
+    path: process.env.NODE_ENV === "production" ? ".env.production" : ".env",
 });
 const express = require("express");
 const mongoose = require("mongoose");
@@ -7,7 +7,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const chalk = require("chalk");
 // const User = require("./models/User");
-const bcryptJs = require("bcryptjs");
+
 const rateLimit = require("express-rate-limit");
 const users = require("./routes/users");
 
@@ -20,20 +20,20 @@ app.use(cors());
 app.use(morgan("dev"));
 
 app.use((req, res, next) => {
-	next();
+    next();
 });
 
 mongoose
-	.connect(process.env.DB)
-	.then(() => {
-		console.log(chalk.blue("connected to mongoDB"));
-	})
-	.catch(() => {
-		console.log(chalk.red("could'nt connected to mongoDB"));
-	});
+    .connect(process.env.DB)
+    .then(() => {
+        console.log(chalk.blue("connected to mongoDB"));
+    })
+    .catch(() => {
+        console.log(chalk.red("could'nt connected to mongoDB"));
+    });
 
 app.use("/api/users", users);
 
 app.listen(PORT, () => {
-	console.log(chalk.greenBright("Server runing on port"), PORT);
+    console.log(chalk.greenBright("Server runing on port"), PORT);
 });
