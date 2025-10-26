9fc1a1d2be61c73f9fdedcddf9fa9e6e0fa707f3
John Doe <johndoe@example.com>
2025-10-26 20:02:59 +0200

    import jwt and lodash

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ const Users = require("../models/Users");
 const bcryptJs = require("bcryptjs");
 const registerSchema = require("../Schema/registerSchema")
 const router = express.Router();
+const jwt = require("jsonwebtoken")
+const _ = require("lodash")
 
 
 router.post("/register", async (req, res) => {
@@ -24,15 +26,18 @@ router.post("/register", async (req, res) => {
 		const hashedPassword = bcryptJs.hashSync(req.body.password, 10)
 
 		// create new user
-		const newUsers = new Users({ ...req.body, password: hashedPassword });
+		const newUser = new Users({ ...req.body, password: hashedPassword });
 		// save user
-		await newUsers.save();
-
-		res.status(201).send("user is created");
-
+		await newUser.save();
 		// create token
+		const data = _.pick(newUser, ["_id", "role", "avatar", "isActive"]);
 
-		// send result
+		data.profile = newUser.profile;
+
+		const token = jwt.sign(data, process.env.SECRET_JWT);
+
+		res.status(201).send(token);
+		// send result 
 	} catch (error) {
 		res.status(500).send(error.message);
 		console.log(req.body.password);
