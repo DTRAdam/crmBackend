7bdc2aa04a2f1f6dd4497fd0c4d27871a029e447
John Doe <johndoe@example.com>
2025-10-26 18:59:53 +0200

    changed the password pattren

--- Schema/registerSchema.js
+++ Schema/registerSchema.js
@@ -2,7 +2,7 @@ const joi = require("joi");
 const registerSchema = joi.object({
     email: joi.string().required(),
     password: joi.string().min(8).required().pattern(
-        /^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$/,
+        /^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{8}$/,
         "the password must be 8 characters length 2 letters in Upper Case 1 Special Character (!@#$&*) 2 numerals (0-9) 3 letters in Lower Case",
     ),
     profile: joi.object({
