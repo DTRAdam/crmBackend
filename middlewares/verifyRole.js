daa955a20169a6debe91d8819031d32f1a35988c
Anis Mhamid <anesmhamed1@gmail.com>
2025-10-28 13:58:34 +0200

    created an handler function to check the user roles for the routes

---
+++ middlewares/verifyRole.js
@@ -0,0 +1,10 @@
+const verifyRole = (roles = []) => {
+	return (req, res, next) => {
+		if (!req.user || !roles.includes(req.user.role)) {
+			return res.status(403).send("Access denied. Insufficient permissions.");
+		}
+		next();
+	};
+};
+
+module.exports = verifyRole;
